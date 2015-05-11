ce1032c7d3a46fc10c247935040c9d3d814463c4
Narain Krishnamurthy <narainsk@cmu.edu>
2015-05-11 13:46:46 -0400

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+jQuery(document).ready(function() {
+  
+
+   $('#carouselHacked').carousel();
+  
+	//this code is for the gmap
+	 var map = new GMaps({
+        el: '#map',
+        lat: -12.043333,
+        lng: -77.028333
+      });
+
+
+      //this code is for smooth scroll and nav selector
+            $(document).ready(function () {
+              $(document).on("scroll", onScroll);
+              
+              //smoothscroll
+              $('a[href^="#"]').on('click', function (e) {
+                  e.preventDefault();
+                  $(document).off("scroll");
+                  
+                  $('a').each(function () {
+                      $(this).removeClass('active');
+                  })
+                  $(this).addClass('active');
+                
+                  var target = this.hash,
+                      menu = target;
+                  $target = $(target);
+                  $('html, body').stop().animate({
+                      'scrollTop': $target.offset().top+2
+                  }, 500, 'swing', function () {
+                      window.location.hash = target;
+                      $(document).on("scroll", onScroll);
+                  });
+              });
+          });
+
+          function onScroll(event){
+              var scrollPos = $(document).scrollTop();
+              $('.navbar-default .navbar-nav>li>a').each(function () {
+                  var currLink = $(this);
+                  var refElement = $(currLink.attr("href"));
+                  //console.log(refElement);
+                  if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                      $('.navbar-default .navbar-nav>li>a').removeClass("active");
+                      currLink.addClass("active");
+                  }
+                  else{
+                      currLink.removeClass("active");
+                  }
+              });
+          }
+     
+
+});
